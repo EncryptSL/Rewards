ffbd37e8e29778bd1169692262edb1186adb60f7
EncryptSL <Rydlos123@gmail.com>
2024-07-27 09:35:05 +0200

    Improvements in code...

--- src/main/kotlin/com/github/encryptsl/rewards/api/menu/OpenGUI.kt
+++ src/main/kotlin/com/github/encryptsl/rewards/api/menu/OpenGUI.kt
@@ -120,8 +120,10 @@ class OpenGUI(private val rewards: Rewards) {
                                         return@asGuiItem player.sendMessage(rewards.locale.translation("messages.rewards.error.missing-discord-link"))
                                     }
                                 } catch (e : DiscordSrvException) {
+                                    playSound(player, errorSound, errorSoundVolume, errorSoundPitch)
                                     return@asGuiItem player.sendMessage(ModernText.miniModernText(e.message ?: e.localizedMessage))
                                 } catch (e : KiraDiscordException) {
+                                    playSound(player, errorSound, errorSoundVolume, errorSoundPitch)
                                     return@asGuiItem player.sendMessage(ModernText.miniModernText(e.message ?: e.localizedMessage))
                                 }
                             }
@@ -145,102 +147,6 @@ class OpenGUI(private val rewards: Rewards) {
                     }
                     container.set(slot, guiItem)
                 }
-
-                /*
-                for (material in Material.entries) {
-                    for (reward in rewards.config.getConfigurationSection("gui.rewards")?.getKeys(false)!!) {
-                        if (!rewards.config.contains("gui.rewards.$reward.display.name"))
-                            return@render player.sendMessage(
-                                rewards.locale.translation("messages.rewards.error.missing-name", Placeholder.parsed("reward", reward))
-                            )
-
-                        if (!rewards.config.contains("gui.rewards.$reward.display.slot"))
-                            return@render player.sendMessage(
-                                rewards.locale.translation("messages.rewards.error.missing-slot", Placeholder.parsed("reward", reward))
-                            )
-
-                        if (!rewards.config.contains("gui.rewards.$reward.display.icon"))
-                            return@render player.sendMessage(
-                                rewards.locale.translation("messages.rewards.error.missing-icon", Placeholder.parsed("reward", reward))
-                            )
-
-                        if (!rewards.config.contains("gui.rewards.$reward.lore"))
-                            return@render player.sendMessage(
-                                rewards.locale.translation("messages.rewards.error.missing-lore", Placeholder.parsed("reward", reward)
-                                ))
-
-                        if (!rewards.config.contains("gui.rewards.$reward.commands"))
-                            return@render player.sendMessage(
-                                rewards.locale.translation("messages.rewards.error.missing-commands", Placeholder.parsed("reward", reward)
-                                ))
-
-                        if (rewards.config.getString("gui.rewards.$reward.display.icon").equals(material.name, true)) {
-                            val name = rewards.config.getString("gui.rewards.$reward.display.name").toString()
-                            val slot = rewards.config.getInt("gui.rewards.$reward.display.slot")
-                            val lore = rewards.config.getStringList("gui.rewards.$reward.lore")
-                            val commands = rewards.config.getStringList("gui.rewards.$reward.commands")
-                            val cooldown = rewards.config.getInt("gui.rewards.$reward.requires.cooldown")
-                            val hasCooldown = rewards.rewardsAPI.hasCooldown(player.uniqueId, reward)
-                            val date = rewards.rewardsAPI.getRemainingDuration(player.uniqueId, reward)
-                            val remaining = date?.let { convertFancyTime(it, pattern) }.toString()
-
-                            val claimSound = rewards.config.getString("gui.click-sounds.claim.type").toString()
-                            val claimSoundPitch = rewards.config.getString("gui.click-sounds.claim.pitch").toString().toFloat()
-                            val claimSoundVolume = rewards.config.getString("gui.click-sounds.claim.volume").toString().toFloat()
-
-                            val errorSound = rewards.config.getString("gui.click-sounds.error.type").toString()
-                            val errorSoundPitch = rewards.config.getString("gui.click-sounds.error.pitch").toString().toFloat()
-                            val errorSoundVolume = rewards.config.getString("gui.click-sounds.error.volume").toString().toFloat()
-
-                            val availableAt = if (hasCooldown)
-                                rewards.locale.translation("messages.rewards.available_at", Placeholder.parsed("available_at", remaining))
-                            else
-                                rewards.locale.translation("messages.rewards.is_available")
-
-                            val guiItem = ItemBuilder.from(itemFactory.item(material, name, lore, availableAt)).asGuiItem { player, _ ->
-                                if (rewards.config.contains("gui.rewards.$reward.requires.permissions")) {
-                                    val permission = rewards.config.getString("gui.rewards.$reward.requires.permissions").toString()
-                                    if (!player.hasPermission(permission)) {
-                                        playSound(player, errorSound, errorSoundVolume, errorSoundPitch)
-                                        return@asGuiItem player.sendMessage(rewards.locale.translation("messages.rewards.error.missing-permissions"))
-                                    }
-                                }
-
-                                if (rewards.config.contains("gui.rewards.$reward.requires.discord")) {
-                                    if (rewards.config.getBoolean("gui.rewards.$reward.requires.discord")) {
-                                        try {
-                                            if (!kiraDiscordHook.isLinked(player) && !discordSrvHook.isLinked(player)) {
-                                                playSound(player, errorSound, errorSoundVolume, errorSoundPitch)
-                                                return@asGuiItem player.sendMessage(rewards.locale.translation("messages.rewards.error.missing-discord-link"))
-                                            }
-                                        } catch (e : DiscordSrvException) {
-                                            return@asGuiItem player.sendMessage(ModernText.miniModernText(e.message ?: e.localizedMessage))
-                                        } catch (e : KiraDiscordException) {
-                                            return@asGuiItem player.sendMessage(ModernText.miniModernText(e.message ?: e.localizedMessage))
-                                        }
-                                    }
-                                }
-
-                                if (hasCooldown) {
-                                    playSound(player, errorSound, errorSoundVolume, errorSoundPitch)
-                                    return@asGuiItem player.sendMessage(rewards.locale.translation("messages.rewards.error.claimed",
-                                        Placeholder.parsed("available_at", remaining)
-                                    ))
-                                }
-
-                                rewards.rewardsAPI.claimReward(player, reward, Duration.ofMinutes(cooldown.toLong()))
-                                rewards.rewardsAPI.receiveReward(player, commands, reward)
-                                playSound(player, claimSound, claimSoundVolume, claimSoundPitch)
-                                player.sendMessage(rewards.locale.translation("messages.rewards.success.claim", TagResolver.resolver(
-                                    Placeholder.parsed("available_at", remaining),
-                                    Placeholder.parsed("reward", name)
-                                )))
-                                player.closeInventory()
-                            }
-                            container.set(slot, guiItem)
-                        }
-                    }
-                }*/
             }
         }.build().open(player)
     }
