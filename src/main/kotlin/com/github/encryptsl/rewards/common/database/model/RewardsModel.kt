a750f033c0a34bb629f7aaa272c2a459d8e63649
EncryptSL <Rydlos123@gmail.com>
2024-09-02 16:01:23 +0200

    Push changes...

--- src/main/kotlin/com/github/encryptsl/rewards/common/database/model/RewardsModel.kt
+++ src/main/kotlin/com/github/encryptsl/rewards/common/database/model/RewardsModel.kt
@@ -1,11 +1,13 @@
 package com.github.encryptsl.rewards.common.database.model
 
+import com.github.encryptsl.rewards.Rewards
 import com.github.encryptsl.rewards.common.database.RewardsSQL
 import com.github.encryptsl.rewards.common.database.tables.RewardTable
 import kotlinx.datetime.Clock
 import kotlinx.datetime.toJavaInstant
 import kotlinx.datetime.toKotlinInstant
 import org.bukkit.entity.Player
+import org.jetbrains.exposed.exceptions.ExposedSQLException
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -13,37 +15,50 @@ import java.time.Duration
 import java.util.*
 import java.util.concurrent.CompletableFuture
 
-class RewardsModel : RewardsSQL {
+class RewardsModel(private val rewards: Rewards) : RewardsSQL {
 
     override fun firstClaim(player: Player, rewardType: String, duration: Duration) {
         CompletableFuture.runAsync {
             transaction {
-                RewardTable.insertIgnore {
-                    it[username] = player.name
-                    it[uuid] = player.uniqueId.toString()
-                    it[reward_type] = rewardType
-                    it[claimed_at] = java.time.Instant.now().plus(duration).toKotlinInstant()
+                try {
+                    RewardTable.insertIgnore {
+                        it[username] = player.name
+                        it[uuid] = player.uniqueId.toString()
+                        it[reward_type] = rewardType
+                        it[claimed_at] = java.time.Instant.now().plus(duration).toKotlinInstant()
+                    }
+                } catch (e : ExposedSQLException) {
+                    rewards.logger.error(e.message ?: e.localizedMessage)
                 }
             }
-        }.join()
+        }
     }
 
     override fun claimReward(uuid: UUID, rewardType: String, duration: Duration) {
         CompletableFuture.runAsync {
             transaction {
-                RewardTable.update({ (RewardTable.uuid eq uuid.toString()) and (RewardTable.reward_type eq rewardType) }) {
-                    it[claimed_at] = java.time.Instant.now().plus(duration).toKotlinInstant()
+                try {
+                    RewardTable.update({ (RewardTable.uuid eq uuid.toString()) and (RewardTable.reward_type eq rewardType) }) {
+                        it[claimed_at] = java.time.Instant.now().plus(duration).toKotlinInstant()
+                    }
+                } catch (e : ExposedSQLException) {
+                    rewards.logger.error(e.message ?: e.localizedMessage)
                 }
             }
-        }.join()
+        }
     }
 
     override fun hasClaimReward(uuid: UUID, rewardType: String): CompletableFuture<Boolean> {
         val future: CompletableFuture<Boolean> = CompletableFuture.supplyAsync {
             transaction {
-                !RewardTable.select(RewardTable.uuid, RewardTable.reward_type)
-                    .where((RewardTable.uuid eq uuid.toString()) and (RewardTable.reward_type eq rewardType))
-                    .empty()
+                try {
+                    !RewardTable.select(RewardTable.uuid, RewardTable.reward_type)
+                        .where((RewardTable.uuid eq uuid.toString()) and (RewardTable.reward_type eq rewardType))
+                        .empty()
+                } catch (e : ExposedSQLException) {
+                    rewards.logger.error(e.message ?: e.localizedMessage)
+                    return@transaction false
+                }
             }
         }
         return future
@@ -52,14 +67,18 @@ class RewardsModel : RewardsSQL {
     override fun hasCooldown(uuid: UUID, rewardType: String): CompletableFuture<Boolean>  {
         val future: CompletableFuture<Boolean> = CompletableFuture.supplyAsync {
             transaction {
-                val cooldown =
-                    RewardTable.select(
-                        RewardTable.claimed_at,
-                        RewardTable.reward_type,
-                        RewardTable.uuid
-                    ).where((RewardTable.uuid eq uuid.toString()) and (RewardTable.reward_type eq rewardType)).firstOrNull()
+                try {
+                    val cooldown =
+                        RewardTable.select(
+                            RewardTable.claimed_at,
+                            RewardTable.reward_type,
+                            RewardTable.uuid
+                        ).where((RewardTable.uuid eq uuid.toString()) and (RewardTable.reward_type eq rewardType)).firstOrNull()
 
-                return@transaction cooldown != null && Clock.System.now().toJavaInstant().isBefore(cooldown[RewardTable.claimed_at].toJavaInstant())
+                    return@transaction cooldown != null && Clock.System.now().toJavaInstant().isBefore(cooldown[RewardTable.claimed_at].toJavaInstant())
+                } catch (e : ExposedSQLException) {
+                    return@transaction false
+                }
             }
         }
         return future
@@ -68,8 +87,12 @@ class RewardsModel : RewardsSQL {
     override fun resetCooldown(uuid: UUID, rewardType: String) {
         CompletableFuture.runAsync {
             transaction {
-                RewardTable.update({ (RewardTable.uuid eq uuid.toString()) and (RewardTable.reward_type eq rewardType) }) {
-                    it[claimed_at] = Clock.System.now()
+                try {
+                    RewardTable.update({ (RewardTable.uuid eq uuid.toString()) and (RewardTable.reward_type eq rewardType) }) {
+                        it[claimed_at] = Clock.System.now()
+                    }
+                } catch (e : ExposedSQLException) {
+                    rewards.logger.error(e.message ?: e.localizedMessage)
                 }
             }
         }.join()
